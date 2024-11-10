ed6e7c221fb560b2a57d24ade3e50d17d84738ca
Puttipong Kamnerdpongpreeda <puttiponghabb@gmail.com>
2024-11-10 23:12:26 +0700

    pong push v.4

--- Frontend/public/js/script.js
+++ Frontend/public/js/script.js
@@ -128,3 +128,13 @@ function showRequest(timeframe) {
         requestList.innerHTML = `<p>ไม่มีคำร้องในช่วงเวลานี้</p>`;
     }
 }
+
+document.getElementById('togglePassword').addEventListener('click', function () {
+    const passwordField = document.getElementById('password');
+    const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordField.setAttribute('type', type);
+
+    // เปลี่ยนข้อความและไอคอน
+    this.textContent = type === 'password' ? ' Show' : ' Hide';
+    this.classList.toggle('show-password');
+});
